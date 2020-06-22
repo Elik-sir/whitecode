d2d7de2ba0f608857a83c8689e086d80d933bbb6
Eldar Kurmanaliev <ekurmanaliev50@gmail.com>
2020-06-22 20:00:12 +0300

    Created page which adding new news

---
+++ src/pages/AddNewsPage/Page.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import TextField from '@material-ui/core/TextField';
+import { addNews } from '../../redux/news/actions';
+import { Link } from 'react-router-dom';
+const AddNewsPage = ({ addNews }, props) => {
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+  const [img, setImg] = useState('');
+  return (
+    <div>
+      <h1>Создание новости</h1>
+      <TextField
+        id='title'
+        label='Название'
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <br />
+      <TextField
+        id='text'
+        label='Текст'
+        onChange={(e) => setText(e.target.value)}
+      />
+      <br />
+      <input
+        type='file'
+        onChange={(e) => {
+          let reader = new FileReader();
+          let file = e.target.files[0];
+
+          reader.onloadend = () => {
+            setImg(reader.result);
+          };
+
+          reader.readAsDataURL(file);
+        }}
+      />
+      <br />
+      <button
+        onClick={() => {
+          if (title && text && img) {
+            addNews({ title, text, img });
+          } else {
+            alert('заполенны не все поля');
+          }
+        }}
+      >
+        <Link to='/news' style={{ color: 'black', textDecoration: 'none' }}>
+          Создать новость!!
+        </Link>
+      </button>
+    </div>
+  );
+};
+const mapDispatchToProps = (dispatch) => ({
+  addNews: (news) => dispatch(addNews(news)),
+});
+export default connect(null, mapDispatchToProps)(AddNewsPage);
